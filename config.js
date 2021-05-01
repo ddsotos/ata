e1e8278d550f148ab9ddff922575c238571c1a65
unknown <tg.dds.marko@gmail.com>
2021-05-01 14:09:14 +0900

    change port

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //ipAddress: 'https://immense-garden-22680.herokuapp.com/'
-    ipAddress: 'http://localhost:8000'
+    ipAddress: 'https://immense-garden-22680.herokuapp.com/'
+    //ipAddress: 'http://localhost:8000'
   };
